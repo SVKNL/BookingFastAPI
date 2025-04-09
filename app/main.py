524751a1f690b5245e5a4209988c8f029f659168
admin <karpovv1994@icloud.com>
2025-04-09 15:26:39 +0500

    added logging examples

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+
+from .logger import logger
 from .routers.table import router as table_router
 from .routers.reservation import router as reservation_router
+
 
 app = FastAPI()
 
 app.include_router(table_router)
 app.include_router(reservation_router)
 
+
+
 if __name__ == "__main__":
     uvicorn.run(app="app.main:app", host="0.0.0.0", port=8000)
+    logger.info("Server started.")
--- app/routers/reservation.py
+++ app/routers/reservation.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from ..database import get_db
+from ..logger import logger
 from ..schemas.reservation import (ReservationCreate,
                                    Reservation as ReservationSchema)
 from ..services.reservation import (create_reservation,
@@ -14,10 +15,14 @@ router = APIRouter()
 @router.post("/reservations/", response_model=ReservationSchema)
 def create_new_reservation(reservation_data: ReservationCreate,
                            db: Session = Depends(get_db)):
-     try:
-         return create_reservation(db=db, reservation_data=reservation_data)
-     except ValueError as e:
-         raise HTTPException(status_code=400, detail=str(e))
+    logger.info("Reservation post endpoint was called")
+    try:
+        return create_reservation(db=db, reservation_data=reservation_data)
+        logger.info("Reservation creation was successful")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        logger.error("Reservation creation didnt happen for unknown reason")
 
 
 @router.get("/reservations/", response_model=list[ReservationSchema])
